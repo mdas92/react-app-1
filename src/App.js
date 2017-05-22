9714d9550cdff141f7c9fe94c838e9455f85b995
unknown <sajjanbalar@gmail.com>
2017-05-22 12:13:28 +0530

    changes by sajjan 1. Removed webpack shit. 2. Removed babelrc and shit. 3.Original App.js is in Src folder, added script in package.json to take that and transform it and put the transformed version in static folder. 4. Removed unused imports.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
-
 ReactDOM.render(
-			<h1>This is index.html</h1>,
-			document.getElementById('example')
+			<h1>Hello World</h1>,
+			document.getElementById('root')
 			);
--- static/App.js
+++ static/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+ReactDOM.render(React.createElement(
+			'h1',
+			null,
+			'Hello World'
+), document.getElementById('root'));
